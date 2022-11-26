c953b7d365e57a77b14cc92f6b5903197ac32a78
Mikhail Sokovykh <misha.sokovih@gmail.com>
2022-11-26 15:56:53 +0300

    Initial commit

---
+++ src/test/java/com/example/git4/Git4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
